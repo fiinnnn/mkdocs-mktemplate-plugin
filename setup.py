dbb1e0db0fee2a88c85ab27609a4894c830bc5d6
Finn Vos <f.vos@dr-flex.de>
2021-12-29 10:06:52 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='mkdocs-mktemplate-plugin',
-    version='1.0.0',
+    version='1.0.1',
     packages=['mktemplate'],
     url='https://github.com/fiinnnn/mkdocs-mktemplate-plugin',
     license='MIT',
